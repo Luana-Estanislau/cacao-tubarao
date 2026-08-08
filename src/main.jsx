2cbd2d99bc680167399ec44574aec77693871cfb
Luana-Estanislau <lestanislau@gmail.com>
2026-08-08 15:30:20 -0300

    feat: route Home at / and move wizard to /reportar (CAC-1)

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './index.css'
+import Home from './Home.jsx'
 import App from './App.jsx'
 import MapaPublico from './MapaPublico.jsx'
 
@@ -9,7 +10,8 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/reportar" element={<App />} />
         <Route path="/mapa" element={<MapaPublico />} />
       </Routes>
     </BrowserRouter>
